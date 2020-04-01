b815ff13089faebaf99174d5acdd74010fe0b5ec
Alexander Kilroy (ale8k) <alexanderkilroy@outlook.com>
2020-04-01 18:17:34 +0100

    Quick packet separation

--- src/Server.ts
+++ src/Server.ts
@@ -70,11 +70,12 @@ class Server {
                 } else if (this.loginProtocolStage === 2) {
 
                     if (test === true) {
-                        const b = Buffer.alloc(14);
+                        let b = Buffer.alloc(0);
                         /**
-                         * 73: Loads map region for Lumbridge
-                         * This actually takes the 8x8 of the 'zone', not the tile. So we / 8.
-                         * Not fully sure what Graham means below... I just follow advice for now.
+                         * 73: Loads map region
+                         * This actually takes the 8x8 of the 'zone', not the tile. A tile consists of 8x8 zones,
+                         * a zone is 8x8 single tiles. So, we get a tile position relative to a zone.
+                         *
                          *
                          * From Graham:
                          * 8x8 tiles = a zone. This is the granularity used in a bunch of packets
@@ -88,77 +89,97 @@ class Server {
                          * When the player is within 16 tiles of the edge the server sends the packet
                          * to reload the area around the player's current zone.
                          */
+                        b = Buffer.alloc(5);
                         b[0] = 73 + this.outStreamCryption.getNextKey();
                         // we need to write little endians here so we can suffix the second byte with
                         // of the first co-ordinate short with +128 (the client removes this on client side)
                         b[1] = 1;
-                        b[2] = 144 + 128;
-                        console.log(b[2]);
-                        b.writeInt16BE(400, 3);
+                        b[2] = 147 + 128;
+                        b.writeInt16BE(403, 3);
+                        socket.write(b);
 
                         /**
                          * 81: Initialise player
                          */
-                        b[5] = 81 + this.outStreamCryption.getNextKey();
+                        b = Buffer.alloc(9);
+                        b[0] = 81 + this.outStreamCryption.getNextKey();
                         // Packet size, VARIABLE_SIZE : SHORT
-                        b.writeInt16BE(6, 6);
-                        /**
-                         * Begin writing bits of player initialisation
-                         */
-                        // update our player or not
-                        this.setBit(b, 8, 7, 1);
-                        // VALUE: 3 - Type 3, update our players plane level
-                        this.setBit(b, 8, 6, 1);
-                        this.setBit(b, 8, 5, 1);
-                        // setting plane level here, 0-3 (we use 0 for now)
-                        this.setBit(b, 8, 4, 0);
-                        this.setBit(b, 8, 3, 0);
-                        // clear awaitig-point queue, i.e., remove our further steps left to do by client. Like when teleing
-                        this.setBit(b, 8, 2, 1);
-                        // is there an update required? (i.e., logged in, update our player)
-                        this.setBit(b, 8, 1, 1);
-                        // x, y co-ords8(20x20)
-                        this.setBit(b, 8, 0, 0);
-                        this.setBit(b, 9, 7, 1);
-                        this.setBit(b, 9, 6, 0);
-                        this.setBit(b, 9, 5, 0);
-                        this.setBit(b, 9, 4, 0);
-                        this.setBit(b, 9, 3, 0);
-                        this.setBit(b, 9, 2, 0);
+                        b.writeInt16BE(6, 1);
 
-                        this.setBit(b, 9, 1, 0);
-                        this.setBit(b, 9, 0, 1);
-                        this.setBit(b, 10, 7, 0);
-                        this.setBit(b, 10, 6, 0);
-                        this.setBit(b, 10, 5, 0);
-                        this.setBit(b, 10, 4, 0);
-                        this.setBit(b, 10, 3, 0);
+                        // update our player or not
+                        this.setBit(b, 3, 7, 1);
+                        // VALUE: 3 - Type 3, update our players plane level
+                        this.setBit(b, 3, 6, 1);
+                        this.setBit(b, 3, 5, 1);
+                        // setting plane level here, 0-3 (we use 0 for now)
+                        this.setBit(b, 3, 4, 0);
+                        this.setBit(b, 3, 3, 0);
+                        // clear awaitig-point queue, i.e., remove our further steps left to do by client. Like when teleing
+                        this.setBit(b, 3, 2, 1);
+                        // is there an update required? (i.e., logged in, update our player)
+                        this.setBit(b, 3, 1, 1);
+                        // x
+                        this.setBit(b, 3, 0, 0);
+                        this.setBit(b, 4, 7, 0);
+                        this.setBit(b, 4, 6, 1);
+                        this.setBit(b, 4, 5, 0);
+                        this.setBit(b, 4, 4, 1);
+                        this.setBit(b, 4, 3, 0);
+                        this.setBit(b, 4, 2, 1);
+                        // y
+                        this.setBit(b, 4, 1, 0);
+                        this.setBit(b, 4, 0, 0);
+                        this.setBit(b, 5, 7, 1);
+                        this.setBit(b, 5, 6, 0);
+                        this.setBit(b, 5, 5, 1);
+                        this.setBit(b, 5, 4, 0);
+                        this.setBit(b, 5, 3, 1);
                         // How many other players the client needs to update,
                         // currently no multiplayer so none,
                         // this will need some considerable thought lol
-                        this.setBit(b, 10, 2, 0);
-                        this.setBit(b, 10, 1, 0);
-                        this.setBit(b, 10, 0, 0);
-                        this.setBit(b, 11, 7, 0);
-                        this.setBit(b, 11, 6, 0);
-                        this.setBit(b, 11, 5, 0);
-                        this.setBit(b, 11, 4, 0);
-                        this.setBit(b, 11, 3, 0);
+                        this.setBit(b, 5, 2, 0);
+                        this.setBit(b, 5, 1, 0);
+                        this.setBit(b, 5, 0, 0);
+                        this.setBit(b, 6, 7, 0);
+                        this.setBit(b, 6, 6, 0);
+                        this.setBit(b, 6, 5, 0);
+                        this.setBit(b, 6, 4, 0);
+                        this.setBit(b, 6, 3, 0);
                         // player list updating, not really sure here. Used wL's 2047 value
-                        this.setBit(b, 11, 2, 1);
-                        this.setBit(b, 11, 1, 1);
-                        this.setBit(b, 11, 0, 1);
-                        this.setBit(b, 12, 7, 1);
-                        this.setBit(b, 12, 6, 1);
-                        this.setBit(b, 12, 5, 1);
-                        this.setBit(b, 12, 4, 1);
-                        this.setBit(b, 12, 3, 1);
-                        this.setBit(b, 12, 2, 1);
-                        this.setBit(b, 12, 1, 1);
-                        this.setBit(b, 12, 0, 1);
+                        this.setBit(b, 6, 2, 1);
+                        this.setBit(b, 6, 1, 1);
+                        this.setBit(b, 6, 0, 1);
+                        this.setBit(b, 7, 7, 1);
+                        this.setBit(b, 7, 6, 1);
+                        this.setBit(b, 7, 5, 1);
+                        this.setBit(b, 7, 4, 1);
+                        this.setBit(b, 7, 3, 1);
+                        this.setBit(b, 7, 2, 1);
+                        this.setBit(b, 7, 1, 1);
+                        this.setBit(b, 7, 0, 1);
                         // initial player update done
-                        console.log(b.toJSON());
+                        // b[8] is empty byte, without it packet is being rejected?
                         socket.write(b);
+
+                        /**
+                         * 253: Send message to client
+                         */
+                        // b = Buffer.alloc(5);
+                        // b[0] = 253 + this.outStreamCryption.getNextKey();
+                        // b.writeInt8(1, 1);
+                        // b.writeInt8(33, 2);
+                        // socket.write(b);
+
+                        /**
+                         * 24: Send message to client
+                         */
+                        b = Buffer.alloc(3);
+                        b[0] = 24 + this.outStreamCryption.getNextKey();
+                        // b.writeInt8(1, 1);
+                        // b.writeInt8(33, 2);
+                        socket.write(b);
+
+
                     }
 
                 }
