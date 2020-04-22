97f2e74bf6bc6488e7776d476a31da647a82fbb6
Alexander Kilroy (ale8k) <alexanderkilroy@outlook.com>
2020-04-22 13:10:36 +0100

    Add bit writing methods
    Preparing for creating P81, here we go... Lol.

---
+++ server_v2/src/game/packets/outgoing/SyncPlayers81.ts
@@ -0,0 +1,14 @@
+/**
+ * This packet is responsible for our local players appearance and location, as well as
+ * surrounding players
+ *
+ * The packet works in 4 steps:
+ *  - Synchronising our local players movement
+ *  - Synchronising other players movment
+ *  - Updating other players apparel
+ *  - Updating our local players apparel
+ * @author ale8k
+ */
+export default class SyncPlayers81 {
+
+}
--- server_v2/src/utils/write-data/WriteBit.ts
+++ server_v2/src/utils/write-data/WriteBit.ts
@@ -2,7 +2,7 @@
  * A helper class to aid us in writing bits to a NodeJS
  * @author ale8k
  */
-export default class WriteByte {
+export default class WriteBit {
     /**
      * Writes bits to a NodeJS Buffer
      * @param buffer the buffer
@@ -10,11 +10,43 @@ export default class WriteByte {
      * @param bit bit index
      * @param value bit value
      */
-    public static writeBit(buffer: Buffer | Uint8Array, i: number, bit: number, value: number): void {
+    public writeBit(b: Buffer | Uint8Array, index: number, bit: number, value: number): void {
         if (value === 0) {
-            buffer[i] &= ~(1 << bit);
+            b[index] &= ~(1 << bit);
         } else {
-            buffer[i] |= (1 << bit);
+            b[index] |= (1 << bit);
         }
     }
+    /**
+     * Converts a number in an array of bits with a specified fixed
+     * size of bits. Please note, if you pass a number that has a minimum bits of
+     * say 4, and try to fix it to size 1. This will not work.
+     * @param num the number to convert into a bit arr
+     * @param amount the amount of bits to emit
+     * @author ale8k
+     */
+    public convertToFixedBitArray(num: number, amount: number): number[] {
+        const bitArr = this.convertToBitArray(num);
+        const bitArrLength = bitArr.length; // remembers the initial length
+
+        if (bitArr.length < amount) {
+            for (let i = 0; i < amount - bitArrLength; i++) {
+                bitArr.unshift(0);
+            }
+        }
+
+        return bitArr;
+    }
+    /**
+     * Takes a whole integer values and parses it into an array of bits
+     * @param number the integer
+     * @author ale8k
+     */
+    private convertToBitArray(number: number): number[] {
+        const numBitArr = (number)?.toString(2).split("").map(numString => {
+            return parseInt(numString);
+        });
+        return numBitArr;
+    }
+
 }
