164be215d91f83d7aca54d90fc3f81c31404ca20
Alexander Kilroy (ale8k) <alexanderkilroy@outlook.com>
2020-04-22 13:18:35 +0100

    Implement bit writing system
    Moved my initial bit writing setup for Packet 81 in Server_v1 to here and wrapped it into a simple class. Not quite perfect,
    but it's a start.

--- server_v2/src/utils/write-data/WriteBit.ts
+++ server_v2/src/utils/write-data/WriteBit.ts
@@ -3,18 +3,25 @@
  * @author ale8k
  */
 export default class WriteBit {
+
     /**
-     * Writes bits to a NodeJS Buffer
-     * @param buffer the buffer
-     * @param i buffer index
-     * @param bit bit index
-     * @param value bit value
+     * Converts a bit array into a byte array and writes the bytes to a given buffer
+     * @param byteIndex the index to start writing the bits from
+     * @param bitArr the array of bits to convert to bytes and write
+     * @param b the buffer to perform the method on
+     * @author ale8k
      */
-    public writeBit(b: Buffer | Uint8Array, index: number, bit: number, value: number): void {
-        if (value === 0) {
-            b[index] &= ~(1 << bit);
-        } else {
-            b[index] |= (1 << bit);
+    public convertBitArrToByteArrAndWrite(byteIndex: number, bitArr: number[], b: Buffer): void {
+        let bitIndex = 7;
+
+        for (let i = 0; i < bitArr.length; i++) {
+            this.writeBit(b, byteIndex, bitIndex, bitArr[i] as number);
+            bitIndex -= 1;
+
+            if (bitIndex <= -1) {
+                bitIndex = 7;
+                byteIndex += 1;
+            }
         }
     }
     /**
@@ -47,6 +54,20 @@ export default class WriteBit {
             return parseInt(numString);
         });
         return numBitArr;
+    }
+    /**
+     * Writes bits to a NodeJS Buffer
+     * @param buffer the buffer
+     * @param i buffer index
+     * @param bit bit index
+     * @param value bit value
+     */
+    private writeBit(b: Buffer | Uint8Array, index: number, bit: number, value: number): void {
+        if (value === 0) {
+            b[index] &= ~(1 << bit);
+        } else {
+            b[index] |= (1 << bit);
+        }
     }
 
 }
