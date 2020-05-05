d050a5400c3c2f94081432feef996fa502ad7424
Alexander Kilroy (ale8k) <alexanderkilroy@outlook.com>
2020-05-05 14:52:55 +0100

    PlayerHandler class creation

---
+++ server_v2/src/handlers/PlayerHandler.ts
@@ -0,0 +1,7 @@
+/**
+ * Static helper class handler for player based actions/events
+ * Is responsible for calculating actions relative to and/or including our local player
+ * @author ale8k
+ */
+export default class PlayerHandler {
+}
