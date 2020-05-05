6c4a4750446f763ed222423dfc6b2298576f6774
Alexander Kilroy (ale8k) <alexanderkilroy@outlook.com>
2020-05-05 14:57:42 +0100

    Method declarations for the PlayerHandler
    Added base declarations with descriptive comments on what they should do ultimately.

--- server_v2/src/handlers/PlayerHandler.ts
+++ server_v2/src/handlers/PlayerHandler.ts
@@ -1,7 +1,26 @@
+import Player from "../game/entities/game/Player";
+
 /**
  * Static helper class handler for player based actions/events
  * Is responsible for calculating actions relative to and/or including our local player
  * @author ale8k
  */
 export default class PlayerHandler {
+    /**
+     * Checks our local players region and returns a list {@link playerList Player[]} of
+     * all the players in that region (excluding our local player)
+     * @param {Player} player local player
+     */
+    public static getPlayersInLocalPlayersRegion(player: Player): void {
+
+    }
+    /**
+     * Filters a player list of players in our region but are not in our local players
+     * visible range
+     * @param {Player} player local player
+     * @param {Player[]} playerList the list of all players in our local players region
+     */
+    public static getPlayersInVisibleRange(player: Player, playerList: Player[]): void {
+
+    }
 }
